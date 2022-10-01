ab41dd4c35f3d0f445b22766152160124d910258
Richard Wunderlich <richard.guenther@web.de>
2022-10-01 13:13:44 +0200

    network: * fix memory leak (close response object) * wait 10 sec for connection to establish * return connection status

--- wifi.py
+++ wifi.py
@@ -5,33 +5,52 @@ import json
 
 
 def connect(ssid, password):
-
+    """
+    connect to wifi network
+    """
     wifi = network.WLAN(network.STA_IF)
     wifi.active(True)
 
     if not wifi.isconnected():
+        print("connect to wifi '{}'".format(ssid))
         wifi.connect(ssid, password)
-        print("Waiting for connection...")
 
-        while not wifi.isconnected():
+        print("waiting for WIFI connection...")
+        wait_connection = 10
+        while not wifi.isconnected() and wait_connection > 0:
             time.sleep(1)
-        print("connected")
+            wait_connection -= 1
 
+        if wifi.isconnected():
+            print("connected")
+            ret = True
+        else:
+            print("connection failed")
+            ret = False
     else:
-        print("Already connected")
+        print("wifi '{}' already connected".format(ssid))
+        ret = True
+
+    return ret
 
 
 def upload_data(url, payload=None):
-    
+    """
+    upload data using HTTP POST request with JSON payload
+    """
+    print("uploading data to '{}'".format(url))
     success = False
     retries = 3
 
     headers = {'Content-Type':'application/json'}
     data = (json.dumps(payload)).encode()
-    
+
     while not success and retries > 0:
         response = urequests.post(url, data=data, headers=headers)
         if response.status_code == 200:
             success = True
+            print("upload successful")
         else:
             retries -= 1
+            print("upload failed with code {}. Try again...".format(response.status_code))
+        response.close()
